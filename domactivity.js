ecf5355fbbe547af0d0c7c5980a5e9305a50a108
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-09-11 22:23:19 -0400

    [Feat] - Added more js

--- domactivity.js
+++ domactivity.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let data = fetch("paintings.json");
-    const jsondata = JSON.parse(data);
-    console.log (jsondata);
+    const jsondata = JSON.parse(content);
+    const paintinglist = document.querySelector(#paintings ul); //this gets the list element under the paintings id section of html
+    function thumbnails(){
+        for(element of jsondata){ //for every element in the data array...
+            const listitem = document.createElement('li'); //creates a list item to go into the list above; got help from https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+            const image = document.createElement('img');
+            image.src = element.id;
+            image.alt = element.title;
+            
+        }
+    }
 });
